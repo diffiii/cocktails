25d169e93810274b398eab2f4ab2b0fb1073ed80
Jan Marek <janwieslaw.marek@gmail.com>
2025-10-11 23:40:45 +0200

    fix: underline GH link for readability

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 export function Footer() {
   return (
     <footer className="border-accent border-t-2">
-      <div className="container mx-auto flex items-center justify-center p-4">
+      <div className="container mx-auto flex items-center justify-center gap-1 p-4 text-muted-foreground">
+        &copy; {new Date().getFullYear()}
         <Link
-          className="text-muted-foreground"
+          className="underline underline-offset-2"
           href="https://github.com/diffiii"
         >
-          &copy; {new Date().getFullYear()} Jan Marek
+          Jan Marek
         </Link>
       </div>
     </footer>
